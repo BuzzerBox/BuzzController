b91c211094d91046464241bd268680eacb4a5b92
Liam T <liam.tirpitz@rwth-aachen.de>
2021-06-21 14:02:59 +0200

    Added simple WDT

--- code/BuzzerBox/BuzzerBox.ino
+++ code/BuzzerBox/BuzzerBox.ino
@@ -1,5 +1,7 @@
 #include <Wire.h>
 #include "portConfig.h"
+#include <avr/wdt.h>
+
 
 boolean isLocked = false;
 boolean standAlone = false;
@@ -7,6 +9,7 @@ uint8_t currentBuzzer = NO_BUZZER;
 
 bool previousState[BUTTON_COUNT] = {HIGH}; 
 void setup() {
+  wdt_enable(WDTO_250MS);
   for (uint8_t i = 0; i < BUTTON_COUNT; i++) {
      pinMode(buttons[i].buttonPin, INPUT_PULLUP);
   }
@@ -64,4 +67,5 @@ void loop() {
     }
     previousState[i] = currentState;
   }
+  wdt_reset();
 }
